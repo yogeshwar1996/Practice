602065cd4c4276bee50d4dc365a831d1a0e2e1f1
yogeshwar1996 <yogeshwartrehan@gmail.com>
2024-01-01 00:31:59 +0530

    Add project to locate and show on the map and search an address and show on map

---
+++ Project Locate and show on Google Map/src/SharePlace.js
@@ -0,0 +1,84 @@
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+import { getCoordsFromAddress, getAddressFromCoords } from "./Utility/Location";
+
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+    this.shareBtn = document.getElementById("share-btn");
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
+    // this.shareBtn.addEventListener('click');
+    addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
+  }
+
+  selectPlace(coordinates, address) {
+    if (this.map) {
+      this.map.render(coordinates);
+    } else {
+      this.map = new Map(coordinates);
+    }
+    this.shareBtn.disabled = false;
+    const sharedLinkInputElement = document.getElementById("share-link");
+    sharedLinkInputElement.value = `${
+      location.origin
+    }/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${
+      coordinates.lng
+    }`;
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your browser - please use a more modern browser or manually enter an address."
+      );
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
+    navigator.geolocation.getCurrentPosition(
+      async (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        this.selectPlace(coordinates, address);
+      },
+      (error) => {
+        modal.hide();
+        alert(
+          "Could not locate you unfortunately. Please enter an address manually!"
+        );
+      }
+    );
+  }
+
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector("input").value;
+    if (!address || address.trim().length === 0) {
+      alert("Invalid address entered - please try again!");
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates, address);
+    } catch (err) {
+      alert(err.message);
+    }
+    modal.hide();
+  }
+}
+
+const placeFinder = new PlaceFinder();
